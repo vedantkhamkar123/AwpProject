4a1fad4bf4e2a7460fabaa637b45ecd2b1905f0f
Vedant Khamkar <153050950+vedantkhamkar123@users.noreply.github.com>
2024-11-25 20:31:14 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Finalyearproject
+{
+    class Product
+    {
+        public string name, image, price;
+        public Product(string na, string img, string pr)
+        {
+            name = na;
+            image = img;
+            price = pr;
+        }
+    }
+}
